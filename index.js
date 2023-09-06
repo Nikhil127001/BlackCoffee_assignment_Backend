c76bf112ba156dda19a50b40cd2b31d79f5cf27d
Nikhil Prajapati <112732049+Nikhil127001@users.noreply.github.com>
2023-09-07 01:28:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app  = express();
+const bodyparser = require('body-parser');
+const apiRoutes = require('./Routes/apiRoutes');
+const cors = require('cors');
+
+require('./Db')
+require('dotenv').config();
+app.use(bodyparser.json());
+app.use(cors());
+app.use(express.urlencoded({extended : true}))
+
+app.use('/api', apiRoutes);
+
+
+app.use((err,req,res,next)=>{
+    res.json({
+        message: err.message
+    })
+})
+const port = 8000;
+
+app.listen(port,() => {
+    console.log(`app listen on port ${port}`);
+})
